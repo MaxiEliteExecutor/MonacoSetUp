2dfdf5506a78cb6ae17ee4c062fd71e8295a1e5e
MaxiEliteExecutor <maxielitecheat@gmail.com>
2025-04-13 21:42:30 +0200

    Update StartWinForms.cs

--- StartWinForms.cs
+++ StartWinForms.cs
@@ -1,9 +1,9 @@
 try
 {
-    await Editor.EnsureCoreWebView2Async(null);
-    Editor.CoreWebView2.Navigate(new Uri($"file:///{Directory.GetCurrentDirectory()}/Editor/monaco.html").ToString());
+    await WEBVIEW2NAME.EnsureCoreWebView2Async(null);
+    WEBVIEW2NAME.CoreWebView2.Navigate(new Uri($"file:///{Directory.GetCurrentDirectory()}/Editor/monaco.html").ToString());
 }
 catch (Exception ex)
 {
-    System.Windows.MessageBox.Show($"Error initializing WebView2: {ex.Message}", "Initialization Error");
+    WEBVIEW2NAME.Windows.MessageBox.Show($"Error initializing WebView2: {ex.Message}", "Initialization Error");
 }
