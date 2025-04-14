8d02b209e304b6beaf5719640cc4d3cf43f33423
MaxiEliteExecutor <maxielitecheat@gmail.com>
2025-04-14 20:05:28 +0200

    Delete StartWinForms.cs

--- StartWinForms.cs
+++
@@ -1,9 +0,0 @@
-try
-{
-    await WEBVIEW2NAME.EnsureCoreWebView2Async(null);
-    WEBVIEW2NAME.CoreWebView2.Navigate(new Uri($"https://getarcexecuter.netlify.app/editor/editor").ToString());
-}
-catch (Exception ex)
-{
-    WEBVIEW2NAME.Windows.MessageBox.Show($"Error initializing WebView2: {ex.Message}", "Initialization Error");
-}
